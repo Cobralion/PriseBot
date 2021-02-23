5d4f76e1416bbfd22904e1e570f66fa95dabaf3a
Maurice Lindner <lindner.maurice@freenet.de>
2021-02-23 18:39:48 +0100

    Projektdateien hinzufügen.

---
+++ PriseBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriseBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
